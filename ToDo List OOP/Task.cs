2eff080bffd7d236084f0e8a5d44835cb4d9aba7
leon <leon@panus.cm>
2024-09-06 08:23:34 +0100

    Initialised the class attributues and create task object

---
+++ ToDo List OOP/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_List_OOP
+{
+    internal class Task
+    {
+        public string Name;
+        public string Date;
+        public string Priority;
+        public string Status;
+
+        public Task(string name, string date, string priority, string status) { 
+        
+            Name = name;
+            Date = date;
+            Priority = priority;
+            Status = status;
+        
+        }
+
+
+
+
+
+
+    }
+}
